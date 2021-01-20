ce36b6008beff00073998277edda1f2f39cc7db9
batenzar <batenzar@gmail.com>
2021-01-21 00:47:22 +0700

    Read input from file

--- App.py
+++ App.py
@@ -9,31 +9,47 @@ import time
 # https://stackabuse.com/how-to-format-dates-in-python/
 # https://www.geeksforgeeks.org/python-program-to-convert-seconds-into-hours-minutes-and-seconds/
 
-url = 'https://www.youtube.com/watch?v=2PPSXonhIck'
-yt = YouTube(url)
-print(url)
-print(yt.title)
-print(yt.author)
-# print(yt.description)
-#print(yt.length)
-print(time.strftime("%H:%M:%S", time.gmtime(yt.length)))
-#print(yt.publish_date)
-print(yt.publish_date.strftime("%b %d, %Y"))
+def getYoutubeDetail(url):
+    """ Read URL for information and generate CSV string. 
 
-# Name,Author,Category,Finished On,Length,Link,Publishing/Release Date,Score /5,Status,Summary,Type
-#"Authentication on the Web (Sessions, Cookies, JWT, localStorage, and more)",Code Realm,,,0:37:05,https://www.youtube.com/watch?v=2PPSXonhIck,8-Nov-18,,,,Video
+    Sample Result ->
+    "Title","Channel",,,0:37:05,https://www.youtube.com/watch?v=xxxxxxx,8-Nov-18,,,,Video
+    
+    :param url: the Youtube video url
+    """
 
-header = "Name,Author,Category,Finished On,Length,Link,Publishing/Release Date,Score /5,Status,Summary,Type"
-csv = "\""+ yt.title + "\","
-csv = csv + "\""+ yt.author + "\","
-csv = csv + ","
-csv = csv + ","
-csv = csv + time.strftime("%H:%M:%S", time.gmtime(yt.length)) + ","
-csv = csv + url + ","
-csv = csv + yt.publish_date.strftime("%d-%b-%y") + ","
-csv = csv + ","
-csv = csv + ","
-csv = csv + ","
-csv = csv + "Video"
-print(header)
-print(csv)
+    yt = YouTube(url)
+
+    # Other information that can be retrieved
+    # print(url)
+    # print(yt.title)
+    # print(yt.author)
+    ## print(yt.description)
+    ## print(yt.length)
+    # print(time.strftime("%H:%M:%S", time.gmtime(yt.length)))
+    ##print(yt.publish_date)
+    # print(yt.publish_date.strftime("%b %d, %Y"))
+
+    
+
+    csv = "\""+ yt.title + "\","
+    csv = csv + "\""+ yt.author + "\","
+    csv = csv + ","
+    csv = csv + ","
+    csv = csv + time.strftime("%H:%M:%S", time.gmtime(yt.length)) + ","
+    csv = csv + url + ","
+    csv = csv + yt.publish_date.strftime("%d-%b-%y") + ","
+    csv = csv + ","
+    csv = csv + ","
+    csv = csv + ","
+    csv = csv + "Video"
+    return csv
+
+def printHeader():
+    csvHeader = "Name,Author,Category,Finished On,Length,Link,Publishing/Release Date,Score /5,Status,Summary,Type"
+    print(csvHeader)
+
+urls = list(open("input.txt"))
+printHeader()
+for url in urls:
+    print(getYoutubeDetail(url))
