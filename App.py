f647c4163418e177a1d3d8c4bea70004bacb4ac9
batenzar <batenzar@users.noreply.github.com>
2021-04-25 22:01:17 +0700

    Use csv package

--- App.py
+++ App.py
@@ -1,6 +1,36 @@
+""" Read URL for information and generate CSV string.
+
+Sample Result ->
+"Title","Channel",,,0:37:05,https://www.youtube.com/watch?v=xxxxxxx,8-Nov-18,,,,Video
+
+:param url: the Youtube video url
+"""
+
 from pytube import YouTube
-import datetime
 import time
+import csv
+
+urls = list(open("input.txt"))
+with open("output.csv", mode="w", encoding="utf-8") as csv_file:
+    writer = csv.DictWriter(csv_file,
+                            ["Name", "Author", "Category", "Finished On", "Length", "Link", "Publishing/Release Date",
+                             "Score /5", "Status", "Summary", "Type"])
+
+    writer.writeheader()
+    for url in urls:
+        yt = YouTube(url)
+
+        # Other information that can be retrieved
+        # print(yt.description)
+
+        writer.writerow({
+            "Name": yt.title,
+            "Author": yt.author,
+            "Length": time.strftime("%H:%M:%S", time.gmtime(yt.length)),
+            "Link": url,
+            "Publishing/Release Date": yt.publish_date.strftime("%d-%b-%y"),
+            "Type": "Video"
+        })
 
 # Ref
 # https://github.com/pytube/pytube
@@ -8,48 +38,5 @@ import time
 # https://python-pytube.readthedocs.io/en/latest/api.html#youtube-object
 # https://stackabuse.com/how-to-format-dates-in-python/
 # https://www.geeksforgeeks.org/python-program-to-convert-seconds-into-hours-minutes-and-seconds/
-
-def getYoutubeDetail(url):
-    """ Read URL for information and generate CSV string. 
-
-    Sample Result ->
-    "Title","Channel",,,0:37:05,https://www.youtube.com/watch?v=xxxxxxx,8-Nov-18,,,,Video
-    
-    :param url: the Youtube video url
-    """
-
-    yt = YouTube(url)
-
-    # Other information that can be retrieved
-    # print(url)
-    # print(yt.title)
-    # print(yt.author)
-    ## print(yt.description)
-    ## print(yt.length)
-    # print(time.strftime("%H:%M:%S", time.gmtime(yt.length)))
-    ##print(yt.publish_date)
-    # print(yt.publish_date.strftime("%b %d, %Y"))
-
-    
-
-    csv = "\""+ yt.title + "\","
-    csv = csv + "\""+ yt.author + "\","
-    csv = csv + ","
-    csv = csv + ","
-    csv = csv + time.strftime("%H:%M:%S", time.gmtime(yt.length)) + ","
-    csv = csv + url + ","
-    csv = csv + yt.publish_date.strftime("%d-%b-%y") + ","
-    csv = csv + ","
-    csv = csv + ","
-    csv = csv + ","
-    csv = csv + "Video"
-    return csv
-
-def printHeader():
-    csvHeader = "Name,Author,Category,Finished On,Length,Link,Publishing/Release Date,Score /5,Status,Summary,Type"
-    print(csvHeader)
-
-urls = list(open("input.txt"))
-printHeader()
-for url in urls:
-    print(getYoutubeDetail(url))
+# https://realpython.com/python-csv/
+# https://stackoverflow.com/questions/904041/reading-a-utf8-csv-file-with-python
